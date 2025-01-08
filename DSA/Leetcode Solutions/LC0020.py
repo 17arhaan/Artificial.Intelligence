534e21b25bb113b15af451f2327d806cbaf5d330
Arhaan Girdhar <17arhaan@gmail.com>
2025-01-08 15:08:05 +0530

    Delete DSA directory

--- DSA/Leetcode Solutions/LC0020.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        for i in range(s):
-            for j in range(1+1,s):
-                if (s[i] == s [j]):
-                    return True
-                else:
-                    return False
